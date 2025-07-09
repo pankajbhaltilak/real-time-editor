1d077c9b3a0d73aea375fc1d52ed12c09fc07fd5
pankajbhaltilak <pankaj.bhaltilak.1@gmail.com>
2025-07-09 21:49:05 +0530

    asgi setup

--- collabeditor/collabeditor/asgi.py
+++ collabeditor/collabeditor/asgi.py
@@ -1,16 +1,18 @@
-"""
-ASGI config for collabeditor project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import editor.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collabeditor.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            editor.routing.websocket_urlpatterns
+        )
+    ),
+})
