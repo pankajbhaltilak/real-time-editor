b70ca18e625519feb308dcb02e60cc07320c2fb8
pankajbhaltilak <pankaj.bhaltilak.1@gmail.com>
2025-07-09 23:38:26 +0530

    Ai Integrated - textblob

--- collabeditor/editor/urls.py
+++ collabeditor/editor/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.signup_view, name='signup'),
+    path('signup/', views.signup_view, name='signup'),
+    path('', views.dashboard, name='dashboard'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('editor/<int:doc_id>/', views.document_editor, name='document_editor'),
+    path('grammar-check/', views.grammar_suggestions, name='grammar_check'),
+    path('create/', views.create_document, name='create_document')
+
 ]
--- collabeditor/editor/views.py
+++ collabeditor/editor/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect ,get_object_or_404
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
+import language_tool_python
+from django.http import JsonResponse
+from .models import Document , DocumentVersion
+import requests
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from textblob import TextBlob
 
 
 def signup_view(request):
@@ -30,6 +37,13 @@ def logout_view(request):
     logout(request)
     return redirect('login')
 
+
+@login_required
+def dashboard(request):
+    documents = Document.objects.filter(owner=request.user)
+    return render(request, 'editor/dashboard.html', {'documents': documents})
+
+
 @login_required
 def document_editor(request, doc_id):
     document = get_object_or_404(Document, pk=doc_id)
@@ -49,3 +63,21 @@ def create_document(request):
         doc = Document.objects.create(title=title, content=content, owner=request.user)
         return redirect('document_editor', doc_id=doc.id)
     return render(request, 'editor/create_document.html')
+
+
+def grammar_suggestions(request):
+    if request.method == 'POST':
+        text = request.POST.get('text', '')
+        blob = TextBlob(text)
+        corrected = str(blob.correct())
+
+        suggestions = []
+        if corrected != text:
+            suggestions.append({
+                'message': 'Spelling/Grammar suggestion',
+                'replacements': [corrected],
+                'offset': 0,
+                'length': len(text)
+            })
+
+        return JsonResponse({'suggestions': suggestions})
